00defd5d593271b2218d20e7c1182dd98bc89b2f
William Spacefire <william@meycup.com>
2021-01-21 23:01:16 -0300

    Post head

--- components/style/post.js
+++ components/style/post.js
@@ -2,8 +2,22 @@ const { makeStyles } = require("@material-ui/core");
 
 module.exports.postStyle = makeStyles({
     cover: {
+        height: "570px",
+        backgroundColor: "#06f",
         color: "white",
-        width: "100%",
-        Height: "570px"
+        padding: "15px",
+        backgroundColor: "rgba(0, 0, 0, 0.36)",
+        backgroundPosition: "center center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "-8px"
+    },
+    post_title: {
+        fontSize: "2.8125rem",
+        fontWeight: 700
+    },
+    post_info: {
+        maxWidth: "650px"
     }
 })
--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,6 @@
 import { postStyle } from "../components/style/post";
 
-const { Container } = require("@material-ui/core");
+const { Container, Typography, Box } = require("@material-ui/core");
 const { default: Copyright } = require("../components/ui/copiryght");
 const { default: Header } = require("../components/ui/header");
 
@@ -11,11 +11,23 @@ function post() {
     return (
         <>
             <Header/>
-            <Container>
-                <div className={classes.cover}>
-                    Hello world
-                </div>
-            </Container>
+                <Box justifyContent="center" flexWrap="wrap" display="flex" className={classes.cover} style={{backgroundImage: "url('/12.jpg')"}}>
+                    <Box>
+                        <div className={classes.post_info}>
+                            <center>
+                                <Typography>
+                                    AUTOMATIZAÇÃO BOT DISCORD JAVASCRIPT NODEJS NPM SHELL TUTORIAL
+                                </Typography>
+                                <Typography>
+                                    3 MESES AGO
+                                </Typography>
+                            </center>
+                            <Typography variant="h1" className={classes.post_title}>
+                                Criando um Bot para o Discord com Node.js – Parte 1
+                            </Typography>
+                        </div>
+                    </Box>
+                </Box>
             <Copyright/>
         </>
     )
