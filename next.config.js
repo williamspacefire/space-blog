405b02822362d0c853d5a016d2ef627f0447bafb
William Spacefire <williamspacefire@users.noreply.github.com>
2021-01-27 12:29:24 -0300

    Changed image provider to Cloudinary

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 module.exports = {
     images: {
-      domains: [
-          'i0.wp.com', 
-          'i1.wp.com', 
-          'i2.wp.com', 
-          'i3.wp.com', 
-          'i4.wp.com', 
-          'i5.wp.com', 
-          'i6.wp.com', 
-          'i7.wp.com']
+      loader: "cloudinary",
+      path: "https://res.cloudinary.com/williamspacefire"
     },
   }
