8616776d406c9d3c9e634c46af1803078e9d63fc
William Spacefire <william@meycup.com>
2021-01-31 13:15:10 -0300

    Format posts

--- components/ui/htmltoreact.js
+++ components/ui/htmltoreact.js
@@ -3,15 +3,15 @@ import NextImage from 'next/image'
 import classes from '../style/post.module.css'
 
 export function H2({ children }) {
-    return <Typography variant="h2">{children}</Typography>
+    return <Typography variant="h2" className={classes.heading}>{children}</Typography>
 }
 
 export function P({ children }) {
-    return <Typography variant="body1">{children}</Typography>
+    return <Typography variant="body1" className={classes.paragraph}>{children}</Typography>
 }
 
 export function Image(props) {
     return (
-        <NextImage src={props.src} quality={100} width={800} height={500} alt={props.alt} layout={"responsive"}/>
+        <figure><NextImage src={props.src} quality={100} width={800} height={500} alt={props.alt} layout={"responsive"}/></figure>
     )
 }
