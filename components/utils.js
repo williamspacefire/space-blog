b8a7ef35cf970cfa6bd15b4a69b09f88315a36b7
William Spacefire <william@meycup.com>
2021-02-04 22:12:53 -0300

    Much better fix for the date format

--- components/utils.js
+++ components/utils.js
@@ -1,21 +1,9 @@
 /**
+ * Format Date().toISOString() to MySQL Datetime
  * 
- * This is a temporary work aroud
- * i'm having a trouble that Date().toISOString()
- * is returning 2020-07-17T00:00:00.000Z
- * stead of 2020-07-17T00:00:00Z
+ * return format YYYY-MM-DDTHH:mm:ssZ
  */
 export function dateFormat(dateString) {
-    var date = new Date(dateString)
-
-    date.toISOString
-
-    const year = date.getFullYear()
-    const month = date.getMonth().toString().padStart(2, "0")
-    const day = date.getDay().toString().padStart(2, "0")
-    const hours = date.getHours().toString().padStart(2, "0")
-    const minutes = date.getMinutes().toString().padStart(2, "0")
-    const seconds = date.getSeconds().toString().padStart(2, "0")
-
-    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`
+    const date = new Date(dateString)
+    return date.toISOString().split(".")[0]+"Z"
 }
