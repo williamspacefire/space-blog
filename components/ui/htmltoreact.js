fa9466c5d324c176c0223219dae4e8c86ec1c5f9
William Spacefire <william@meycup.com>
2021-03-09 22:31:41 -0300

    Fix: Potentially unsafe external link

--- components/ui/htmltoreact.js
+++ components/ui/htmltoreact.js
@@ -25,7 +25,7 @@ export function Link(props) {
     const urlHost = urlParse(props.href)
 
     if (urlHost.host != "compilado.xyz") {
-        result = <a href={props.href} title={props.title} target="_blank">{props.children}</a>
+        result = <a rel="noopener noreferrer" href={props.href} title={props.title} target="_blank">{props.children}</a>
     } else {
         result = <NextLink href={props.href} passHref><a title={props.title} rel="noopener noreferrer">{props.children}</a></NextLink>
     }
